ec0584bbe9efb3afdd8c1f84c26932a07c361766
TNTimo1 <94600645+TNTimo1@users.noreply.github.com>
2023-06-02 10:27:37 +0200

    UserController gemaakt

---
+++ ZuydWorld/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using ZuydWorld.Models;
+
+namespace ZuydWorld.Controllers
+{
+    public class UserController : Controller
+    {
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
