424abb0a364a94f5b77549dbec7f310c932025af
AndyErmers <96414877+AndyErmers@users.noreply.github.com>
2023-06-23 10:44:17 +0200

    Maak controller Game aan

---
+++ ZuydWorld/Controllers/GameController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZuydWorld.Controllers
+{
+    public class GameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Default()
+        {
+            return View();
+        }
+    }
+}
