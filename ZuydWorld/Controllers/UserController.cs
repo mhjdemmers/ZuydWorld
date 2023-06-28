2d5243cfe445930a3912ab530d1572e4ebf4348e
TNTimo1 <94600645+TNTimo1@users.noreply.github.com>
2023-06-28 18:54:39 +0200

    Registration view bijgewerkt

--- ZuydWorld/Controllers/UserController.cs
+++ ZuydWorld/Controllers/UserController.cs
@@ -18,5 +18,7 @@ namespace ZuydWorld.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        
     }
 }
